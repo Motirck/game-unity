e75e08e9a23b43990c5efacaab7a03f0f5e19306
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-11-19 19:05:27 -0300

    Added new scene that is called when player win the game

--- Game-Unity/Assets/Scripts/Victory.cs
+++ Game-Unity/Assets/Scripts/Victory.cs
@@ -1,10 +1,22 @@
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Victory : MonoBehaviour
 {
+    // 3.5 seconds
+    public float delay = 3.5f;
+
     public void OnTriggerEnter(Collider other)
     {
         // Play music victory
         GetComponent<AudioSource>().Play();
+
+        Invoke("DelayedInChangingScene", delay);
+    }
+
+    void DelayedInChangingScene()
+    {
+        SceneManager.LoadScene("Winner");
     }
 }
