c24ef72f363657370cd4e22a9e060267e3eb7716
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-11-20 19:11:29 -0300

    Added countdown song

--- Game-Unity/Assets/Scripts/CountdownGame.cs
+++ Game-Unity/Assets/Scripts/CountdownGame.cs
@@ -7,9 +7,15 @@ public class CountdownGame : MonoBehaviour
     public Text displayCountdown;
 
     public float count = 120.0f;
+    public int countSong = 0;
 
     void Update()
     {
+        if (count <= 11.5f && countSong == 0)
+        {
+            countSong++;
+            PlaySongCountdown();
+        }
         if (count > 0.0f)
         {
             count -= Time.deltaTime;
@@ -20,5 +26,11 @@ public class CountdownGame : MonoBehaviour
             displayCountdown.text = "Time's up";
             SceneManager.LoadScene("GameOver");
         }
+    }
+
+    public void PlaySongCountdown()
+    {
+        print("Entrou no 10");
+        GetComponent<AudioSource>().Play();
     }
 }
